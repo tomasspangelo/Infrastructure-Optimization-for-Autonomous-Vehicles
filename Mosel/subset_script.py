0e2d3e71bddf1b862f823aa003760a7d195f1e56
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-03-30 15:34:42 +0200

    Added comments, fixed formatting according to PEP8

--- Mosel/subset_script.py
+++ Mosel/subset_script.py
@@ -1,13 +1,12 @@
 import itertools
 import numpy as np
-import sys
 
 nCities = 0
 nCore = 0
 Z = 0
 
-
-f = open("AutonomaxData.txt","r")
+# Read number of cities from AutonomaxData.txt
+f = open("AutonomaxData.txt", "r")
 line = f.readline()
 while line:
     line = str(line)
@@ -16,7 +15,8 @@ while line:
         break
 f.close()
 
-f = open("autonomax.mos","r")
+# Read number of cities in core net and cyclic/path from mosel parameter environment
+f = open("autonomax.mos", "r")
 line = f.readline()
 while line:
     line = str(line)
@@ -28,26 +28,31 @@ while line:
     line = f.readline()
 
 f.close()
-if Z==1:
-    maxSize=int(np.floor(nCore/2))
-if Z==0:
-    maxSize=nCore-1
-nSubsets=0
-cities = np.arange(1, nCities + 1)
 
+# maxSize of subsets is dependent on whether the core net is cyclic or not
+if Z == 1:
+    maxSize = int(np.floor(nCore / 2))
+if Z == 0:
+    maxSize = nCore - 1
+
+nSubsets = 0
+cities = np.arange(1, nCities + 1)  # Array containing 1, 2 ... nCities
+
+# Creates subsets and writes to subsets.txt
 f = open("subsets.txt", "w")
 f.write("Subsets: [\n")
-for i in range(2,maxSize+1):
-    subsets=(list(itertools.combinations(cities, i)))
+for i in range(2, maxSize + 1):
+    subsets = (list(itertools.combinations(cities, i)))
     for set in subsets:
-        nSubsets+=1
-        line=""
+        nSubsets += 1
+        line = ""
         for c in cities:
             if c in set:
-                line+=str(1)+"\t"
-            else: line += str(0)+"\t"
-        f.writelines(line+"\n")
+                line += str(1) + "\t"
+            else:
+                line += str(0) + "\t"
+        f.writelines(line + "\n")
 f.writelines("] \n")
-f.writelines("nSubsets: "+str(nSubsets))
+f.writelines("nSubsets: " + str(nSubsets))
 
 f.close()
